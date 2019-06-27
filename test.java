f2267396be9f70956f615ffaa4cde9b3a4556466
NaumTodolliALG2019 <51859124+NaumTodolliALG2019@users.noreply.github.com>
2019-06-27 16:43:26 +0200

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+package web;
+
+
+public class Test {
+	 public static void main(String[] args)
+	    {
+	        Crawler1 spider = new Crawler1 ();
+	        spider.search("https://www.google.com/");
+	    }
+}
